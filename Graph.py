3c0ab3a3012532cd06fab25b9edfdd904797664f
alan.dennis@gmail.com <alan.dennis@gmail.com>
2021-07-22 18:55:37 +0000

    pizza

--- Graph.py
+++ Graph.py
@@ -1,6 +1,7 @@
 # Databricks notebook source
 # MAGIC %md 
 # MAGIC # What is a Graph?
+# MAGIC For our purposes we are discussing directed graphs.  More generally, graphs are not necessarily directed.
 # MAGIC A collection of vertices/nodes and edges connecting them.
 # MAGIC 
 # MAGIC ![](https://raw.githubusercontent.com/alandennis1024/graphdatabase/main/images/SimpleGraph.png)
@@ -11,8 +12,7 @@
 # MAGIC - Edges have a direction. They start at one vertex and terminate at another.
 # MAGIC - Attributes are on both vertices and edges.  Examples include id, name, weight, and so on.
 # MAGIC 
-# MAGIC For a detailed discussion of graphs consider 
-# MAGIC 
+# MAGIC For a detailed discussion of graphs and graph theory -  
 # MAGIC Cusack, C. A., & Santos, D. A. (2021). An Active Introduction to Discrete Mathematics and Algorithms. GNU. https://cusack.hope.edu/Notes/?Instructor=Books 
 
 # COMMAND ----------
@@ -34,8 +34,15 @@
 
 # COMMAND ----------
 
-vertices = spark.sql("select * from vertices")
-display(vertices)
+# MAGIC %md 
+# MAGIC # What is GraphFrames
+# MAGIC - Combination of graph processing from GraphX with Spark DataFrames
+# MAGIC - Essentially contains two DataFrames, one for edges and one for verticies
+
+# COMMAND ----------
+
+v = spark.sql("select * from vertices")
+display(v)
 
 # COMMAND ----------
 
@@ -44,13 +51,13 @@ display(vertices)
 
 # COMMAND ----------
 
-vertices = vertices.withColumnRenamed("Node","id")
-display(vertices)
+v = v.withColumnRenamed("Node","id")
+display(v)
 
 # COMMAND ----------
 
-edges = spark.sql("select * from edges")
-display(edges)
+e = spark.sql("select * from edges")
+display(e)
 
 # COMMAND ----------
 
@@ -59,21 +66,28 @@ display(edges)
 
 # COMMAND ----------
 
-edges = edges.withColumnRenamed("From","src").withColumnRenamed("To","dst")
-display(edges)
+e = e.withColumnRenamed("From","src").withColumnRenamed("To","dst")
+display(e)
 
 # COMMAND ----------
 
 from graphframes import *
-g = GraphFrame(vertices, edges)
-print(g)
+g = GraphFrame(v, e)
+
+# COMMAND ----------
+
+display(g.edges)
+
+# COMMAND ----------
+
+display(g.vertices)
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC # Example - Prerequisites in undergraduate programs
 # MAGIC Typically, classes in undergraduate are gated by prerequisites.
-# MAGIC - Example: OO Programming -> Video Game Programming
+# MAGIC - Example: Video Game Programming requires completion of OO Programming
 # MAGIC - One class often unlocks multiple higher numbered classes
 # MAGIC - Important for students to take the right classes (and gain the right knowledge) at the right time
 
@@ -83,7 +97,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 def PlotGraph(graph):
-  G = nx.from_pandas_edgelist(g.edges.toPandas(),'src','dst')
+  G = nx.from_pandas_edgelist(graph.edges.toPandas(),'src','dst')
   plt.figure(1,figsize=(8,8)) 
   nx.draw(G,with_labels = True, arrows = True, arrowsize=20,edge_color = 'b', arrowstyle='fancy' )
   plt.show()
@@ -115,7 +129,7 @@ display(g.triangleCount().filter("count >0"))
 # COMMAND ----------
 
 from pyspark.sql.functions import desc
-display(g.inDegrees.join(vertices,"id").orderBy(desc("inDegree")))
+display(g.inDegrees.join(g.vertices,"id").orderBy(desc("inDegree")))
 
 # COMMAND ----------
 
@@ -124,7 +138,7 @@ display(g.inDegrees.join(vertices,"id").orderBy(desc("inDegree")))
 
 # COMMAND ----------
 
-display(g.outDegrees.join(vertices,"id").orderBy(desc("outDegree")))
+display(g.outDegrees.join(g.vertices,"id").orderBy(desc("outDegree")))
 
 # COMMAND ----------
 
@@ -133,7 +147,55 @@ display(g.outDegrees.join(vertices,"id").orderBy(desc("outDegree")))
 
 # COMMAND ----------
 
-display(g.degrees.join(vertices,"id").orderBy(desc("degree")))
+display(g.degrees.join(g.vertices,"id").orderBy(desc("degree")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Graph Algorithms
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Connected Components
+# MAGIC From GraphFrames documentation 
+# MAGIC 
+# MAGIC         Computes the connected component membership of each vertex and returns a graph with each vertex assigned a component ID.
+
+# COMMAND ----------
+
+!mkdir /tmp/checkpoints
+
+# COMMAND ----------
+
+# must set checkpoing directory before calling connectedComponents
+sc.setCheckpointDir("/tmp/checkpoints")
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+result = g.connectedComponents()
+display(result)
+
+# COMMAND ----------
+
+groupedByComponent = result.groupBy(col("component")).count().orderBy(desc("count"))
+display(groupedByComponent)
+
+# COMMAND ----------
+
+disconnectedVertices =groupedByComponent.filter("count = 1").join(g.vertices,g.vertices.id ==groupedByComponent.component ) 
+display(disconnectedVertices)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC We have several isollated nodes. We can remove them with dropIsolatedVertices()
+
+# COMMAND ----------
+
+justConnectedG =g.dropIsolatedVertices()
+display(justConnectedG.vertices)
 
 # COMMAND ----------
 
@@ -148,7 +210,18 @@ display(g.degrees.join(vertices,"id").orderBy(desc("degree")))
 result = g.stronglyConnectedComponents(maxIter=5)
 #result.select("id", "Name").orderBy("component").show()
 display(result.groupBy(col("component")).count().orderBy(desc("count")))
-#Note there are no vertices that are part of a subgraph that are stronlgy connected
+#Note there are no vertices that are part of a subgraph that are strongly connected
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ## Breadth-first search (BFS)
+# MAGIC Breadth-first search (BFS) finds the shortest path(s) from one vertex (or a set of vertices) to another vertex (or a set of vertices). The beginning and end vertices are specified as Spark DataFrame expressions.
+
+# COMMAND ----------
+
+paths = g.bfs("Name = 'ITSS 230'", "Name ='ITSS 439'")
+display(paths)
 
 # COMMAND ----------
 
@@ -159,12 +232,31 @@ display(result.groupBy(col("component")).count().orderBy(desc("count")))
 # COMMAND ----------
 
 results = g.pageRank(resetProbability=0.15, tol=0.01)
-display(results.vertices.select("id","pagerank").join(vertices,"id").orderBy(desc("pagerank")))
+display(results.vertices.select("id","pagerank").join(g.vertices,"id").orderBy(desc("pagerank")))
 
 
 # COMMAND ----------
 
 display(results.edges.select("src", "dst", "weight").orderBy(desc("weight")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Label Proagation Algorithm (LPA)
+# MAGIC 
+# MAGIC Run static Label Propagation Algorithm for detecting communities in networks.
+# MAGIC 
+# MAGIC Each node in the network is initially assigned to its own community. At every superstep, nodes send their community affiliation to all neighbors and update their state to the **mode** community affiliation of incoming messages.
+# MAGIC 
+# MAGIC LPA is a standard community detection algorithm for graphs. It is very inexpensive computationally, although (1) convergence is not guaranteed and (2) one can end up with trivial solutions (all nodes are identified into a single community).
+
+# COMMAND ----------
+
+g2 = GraphFrame(v.withColumnRenamed("Label","Classname"), e)
+result = g2.labelPropagation(maxIter=5)
+print(result.columns)
+display(result.orderBy(col('label')))
+#result.select("id", "label").show()
 
 # COMMAND ----------
 
@@ -278,6 +370,67 @@ display(chain5Filter)
 # COMMAND ----------
 
 display(chain5Filter.select("a.Label", "b.Label", "c.Label","d.Label", "e.Label"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Where to learn more
+# MAGIC - GraphFrames documentation https://graphframes.github.io/graphframes/docs/_site/index.html
+# MAGIC - GraphX documentation https://spark.apache.org/docs/latest/graphx-programming-guide.html
+
+# COMMAND ----------
+
+pizzav = spark.sql("select * from pizzav")
+pizzae = spark.sql("select * from pizzae")
+pizzaGraph = GraphFrame(pizzav,pizzae)
+PlotGraph(pizzaGraph)
+
+# COMMAND ----------
+
+display(pizzaGraph.vertices)
+
+# COMMAND ----------
+
+display(pizzaGraph.edges)
+
+
+# COMMAND ----------
+
+for x in pizzaGraph.vertices.collect():
+  print(x.Name)
+
+
+# COMMAND ----------
+
+def PlotGraphWithLabels(graph,vertexLabel,edgeLabel):
+  G = nx.DiGraph()
+  for x in pizzaGraph.edges.collect():
+    src = x["src"]
+    dst = x["dst"]
+    weight = x["weight"]
+    #print(src,dst,weight)
+    G.add_edge(src,dst,weight=weight)
+  plt.figure(1,figsize=(8,8)) 
+  pos = nx.spring_layout(G)
+  #nx.draw(G,with_labels = True, arrows = True, arrowsize=20,edge_color = 'b', arrowstyle='fancy' )
+  node_labels = nx.get_node_attributes(G,vertexLabel)
+  print(node_labels)  
+  nx.draw_networkx_labels(G, pos, labels = node_labels)
+  
+  edge_labels = nx.get_edge_attributes(G,edgeLabel)
+  nx.draw_networkx_edge_labels(G, pos, labels = edge_labels)
+  nx.draw_networkx_edges(G, pos, edge_color='green',     arrowstyle='-|>')
+  plt.show()
+PlotGraphWithLabels(pizzaGraph,"Name","weight")
+
+# COMMAND ----------
+
+|display(pizzaGraph.edges)
+
+
+# COMMAND ----------
+
+display(pizzaGraph.edges.groupBy("src").sum("weight"))
 
 # COMMAND ----------
 
